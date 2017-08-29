339ee76f4fa0aba39876aeb9e29996fd12772564
marekchen <marek@163.com>
2017-08-29 11:56:11 +0800

    add droiv build

---
+++ src/build/Android.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const chalk = require('chalk');
+const child_process = require('child_process');
+const {
+  Config,
+  androidConfigResolver
+} = require('../utils/config');
+const utils = require('../utils');
+const copy = require('recursive-copy');
+const startJSServer = require('../run/server');
+
+function buildAndroid() {
+  utils.buildJS()
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        copy('./dist/', 'platforms/android/app/src/main/assets/dist', {
+          overwrite: true
+        }, function (err) {
+          if (err) return reject(err);
+          else resolve();
+        });
+      });
+    })
+    .then(() => {
+      startJSServer();
+      return;
+    })
+    .then(prepareAndroid)
+    .then(resolveConfig)
+    .then(buildApp)
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+}
+
+function prepareAndroid() {
+  return new Promise((resolve, reject) => {
+    const rootPath = process.cwd();
+
+    if (!utils.checkAndroid(rootPath)) {
+      console.log();
+      console.log(chalk.red('  Android project not found !'));
+      console.log();
+      console.log(`  You should run ${chalk.blue('weexpack init')} first`);
+      reject();
+    }
+
+    console.log();
+    console.log(` => ${chalk.blue.bold('Will build Android app')}`);
+
+    // change working directory to android
+    process.chdir(path.join(rootPath, 'platforms/android'));
+    if (!process.env.ANDROID_HOME) {
+      console.log();
+      console.log(chalk.red('  Environment variable $ANDROID_HOME not found !'));
+      console.log();
+      console.log(`  You should set $ANDROID_HOME first.`);
+      console.log(`  See ${chalk.cyan('http://stackoverflow.com/questions/19986214/setting-android-home-enviromental-variable-on-mac-os-x')}`);
+      reject();
+    }
+    resolve({
+      rootPath
+    });
+  });
+}
+
+function resolveConfig({
+  rootPath
+}) {
+  let androidConfig = new Config(androidConfigResolver, path.join(rootPath, 'android.config.json'));
+  return androidConfig.getConfig().then((config) => {
+    androidConfigResolver.resolve(config);
+    return;
+  });
+}
+
+function buildApp() {
+  return new Promise((resolve, reject) => {
+    console.log(` => ${chalk.blue.bold('Building app ...')}`);
+    try {
+      let clean = ' clean';
+      child_process.execSync(process.platform === 'win32' ? `call gradlew.bat${clean} assemble` : `./gradlew${clean} assemble`, {
+        encoding: 'utf8',
+        stdio: [0, 1, 2]
+      });
+    } catch (e) {
+      reject();
+    }
+    resolve();
+  });
+}
+
+module.exports = buildAndroid;
---
+++ src/build/iOS.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const chalk = require('chalk');
+const child_process = require('child_process');
+const fs = require('fs');
+const utils = require('../utils');
+const {
+  Config,
+  iOSConfigResolver
+} = require('../utils/config');
+const startJSServer = require('../run/server');
+
+function buildIOS() {
+  utils.checkAndInstallForIosDeploy()
+    .then(utils.buildJS)
+    .then(() => {
+      return utils.exec('rsync  -r -q ./dist/* platforms/ios/bundlejs/');
+    })
+    .then(() => {
+      startJSServer();
+      return;
+    })
+    .then(prepareIOS)
+    .then(installDep)
+    .then(resolveConfig)
+    .then(doBuild)
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+}
+
+function prepareIOS() {
+  return new Promise((resolve, reject) => {
+    const rootPath = process.cwd();
+    if (!utils.checkIOS(rootPath)) {
+      console.log();
+      console.log(chalk.red('  iOS project not found !'));
+      console.log();
+      console.log(`  You should run ${chalk.blue('weexpack init')} first`);
+      reject();
+    }
+
+    process.chdir(path.join(rootPath, 'platforms/ios'));
+    const xcodeProject = utils.findXcodeProject(process.cwd());
+    if (xcodeProject) {
+      console.log();
+      resolve({
+        rootPath
+      });
+    } else {
+      console.log();
+      console.log(`  ${chalk.red.bold('Could not find Xcode project files in ios folder')}`);
+      console.log();
+      console.log(`  Please make sure you have installed iOS Develop Environment and CocoaPods`);
+      console.log(`  See ${chalk.cyan('http://alibaba.github.io/weex/doc/advanced/integrate-to-ios.html')}`);
+      reject();
+    }
+  });
+}
+
+function installDep({
+  rootPath
+}) {
+  return new Promise((resolve, reject) => {
+    try {
+      console.log(` => ${chalk.blue.bold('pod install')}`);
+      let child = child_process.exec('pod install --no-repo-update', {
+        encoding: 'utf8'
+      }, function () {
+        resolve({
+          rootPath
+        });
+      });
+      child.stdout.pipe(process.stdout);
+      child.stderr.pipe(process.stderr);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function resolveConfig({
+  rootPath
+}) {
+  let iOSConfig = new Config(iOSConfigResolver, path.join(rootPath, 'ios.config.json'));
+  return iOSConfig.getConfig().then((config) => {
+    iOSConfigResolver.resolve(config);
+    fs.writeFileSync(path.join(process.cwd(), 'bundlejs/index.js'), fs.readFileSync(path.join(process.cwd(), '../../dist', config.WeexBundle.replace(/\.we$/, '.js'))));
+    return {};
+  });
+}
+
+function doBuild() {
+  return new Promise((resolve) => {
+    let child = child_process.exec(path.join(__dirname, 'lib/cocoapods-build') + ' . Debug', {
+      encoding: 'utf8'
+    }, function () {
+      console.log('Build success!');
+      resolve();
+    });
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+  });
+}
+
+module.exports = buildIOS;
